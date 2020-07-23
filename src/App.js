d3edb2f7a9eb31cf5d4cac7423f831ef0d0f9828
unknown <shanpirana@gmail.com>
2020-07-23 20:38:27 +0530

    login route

--- src/App.js
+++ src/App.js
@@ -9,21 +9,21 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home'
 import CheckOutPage from './pages/CheckOutPage';
-
+import SignUpPage from './pages/SignupPage';
+import LoginPage from './pages/LoginPage';
 const App = () => {
   return (
     <>
       <Router>
         <Header />
-
         <Switch>
+          <Route path='/login' exact component={LoginPage} />
+          <Route path='/signup' exact component={SignUpPage} />
           <Route path='/' exact component={Home} />
           <Route path='/product' exact component={ProductViewPage} />
           <Route path='/checkout' exact component={CheckOutPage} />
-          <Home />
+
         </Switch>
-
-
         <Footer />
       </Router>
     </>
